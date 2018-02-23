3664d8847106504c03cf80fbb1d25c0472852a5c
liusq23 <liusq23@ziroom.com>
2018-02-23 15:31:22 +0800

    init

---
+++ asura-base-autoconfig/src/main/java/com/asura/base/autoconfig/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * @FileName: package-info.java
+ * @Package: com.asura.base.autoconfig
+ * @author liusq23
+ * @created 2018/2/23 下午3:29
+ * <p>
+ * Copyright 2018 asura
+ */
+package com.asura.base.autoconfig;
---
+++ asura-base-sample/src/main/java/com/asura/base/sample/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * @FileName: package-info.java
+ * @Package: com.asura.base.sample
+ * @author liusq23
+ * @created 2018/2/23 下午3:30
+ * <p>
+ * Copyright 2018 asura
+ */
+package com.asura.base.sample;
